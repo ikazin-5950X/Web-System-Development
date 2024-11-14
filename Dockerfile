0b1249a9c595fa64066162a3dd3fbf66768fa707
ikazin-5950X <ikazinyuukyan@gmail.com>
2024-11-14 00:18:08 +0000

    2024_11_07

--- Dockerfile
+++ Dockerfile
@@ -19,4 +19,5 @@ RUN docker-php-ext-install redis
 
 RUN install -o www-data -g www-data -d /var/www/upload/image/
 
-RUN echo -e "post_max_size = 5M\nupload_max_filesize = 5M" >> ${PHP_INI_DIR}/php.ini
+# RUN echo -e "post_max_size = 5M\nupload_max_filesize = 5M" >> ${PHP_INI_DIR}/php.ini
+COPY ./php.ini ${PHP_INI_DIR}/php.ini
--- public/login.php
+++ public/login.php
@@ -17,44 +17,15 @@ if (!empty($_POST['email']) && !empty($_POST['password'])) {
   }
 
   // パスワードが正しいかチェック
-  $password_hash = mb_substr($user['password'], 0, 64); //0~64文字分がhash
-  $salt = mb_substr($user['password'], 64, 64); //64~64文字分がsalt
-  // ストレッチング処理
-  $stretched_hash = $_POST['password'] . $salt;
-  for ($i = 0; $i < 100; $i++) {
-    $stretched_hash = hash('sha256', $stretched_hash);
-  }
-  $correct_password = $stretched_hash === $password_hash;
+  $correct_password = password_verify($_POST['password'], $user['password']);
   if (!$correct_password) {
     // パスワードが間違っていれば、処理を中断しエラー用クエリパラメータ付きのログイン画面URLにリダイレクト
     header("HTTP/1.1 302 Found");
     header("Location: ./login.php?error=1");
     return;
   }
-
-  # ここからセッションの独自実装 (詳細は後期第2回授業参照) ##############
-  // セッションIDの取得(なければ新規で作成&設定)
-  $session_cookie_name = 'session_id';
-  $session_id = $_COOKIE[$session_cookie_name] ?? base64_encode(random_bytes(64));
-  if (!isset($_COOKIE[$session_cookie_name])) {
-    setcookie($session_cookie_name, $session_id);
-  }
-  // 接続 (redisコンテナの6379番ポートに接続)
-  $redis = new Redis();
-  $redis->connect('redis', 6379);
-  // Redisにセッション変数を保存しておくキー
-  $redis_session_key = "session-" . $session_id;
-  // Redisからセッションのデータを読み込み
-  // 既にセッション変数(の配列)が何かしら格納されていればそれを，なければ空の配列を $session_values変数に保存
-  $session_values = $redis->exists($redis_session_key)
-    ? json_decode($redis->get($redis_session_key), true)
-    : [];
   // セッションにログインできた会員情報の主キー(id)を設定
-  $session_values["login_user_id"] = $user['id'];
-  // セッションをRedisに保存
-  $redis->set($redis_session_key, json_encode($session_values));
-  # セッションここまで ###################################################
-
+  $_SESSION["login_user_id"] = $user['id'];
   // ログインが成功したらログイン完了画面にリダイレクト
   header("HTTP/1.1 302 Found");
   header("Location: ./login_finish.php");
